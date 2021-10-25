f4ed2dc03ce8e6de0648ef8bae91bfa5fa507282
Davi Kunzendorff <87789642+TheDwinRedhead@users.noreply.github.com>
2021-10-25 15:33:13 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+const linksSocialMedia = {
+  github: 'TheDwinRedhead',
+  facebook: 'Davi.Kunzendorff',
+  //Davi.Kunzendorff
+  instagram: 'kunzendorff_davi'
+}
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `https://www.${social}.com/${linksSocialMedia[social]}`
+  }
+}
+
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${linksSocialMedia.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userGit.href = data.html_url
+      userPhoto.src = data.avatar_url
+      userLogin.textContent = data.login
+
+    })
+}
+
+getGitHubProfileInfos()
